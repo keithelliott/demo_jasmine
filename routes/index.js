914471daace70bb89ae6f5c5f751156bfa76eca8
Keith Elliott <mrkeithelliott@gmail.com>
2012-08-23 07:00:42 -0400

    stage files for step one

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
-var geocoder = require('geocoder'),
-	xmlconverter = require('xml2js'),
-	parser = new xmlconverter.Parser(),
-	http = require("http")
-	ZWSID = 'X1-ZWz1decm08q58r_4g4th';
+var GeoStats = require('../geostats.js').GeoStats,
+		geo = new GeoStats({name: 'My Default Search'});
 	
 exports.index = function(req, res){
 	var pageVars = {};
@@ -17,105 +14,6 @@ exports.index = function(req, res){
   res.render('index', pageVars);
 };
 
-exports.geocode = function(req, res){
-	console.log('geocode called');
-	var address = req.params.address;
-	//console.log(req.params);
-//	console.log(address);
-	geocoder.geocode(address, function(err,data){
-		if(err){
-			console.log(err);
-		}
-		else{
-			console.dir(data.results[0]);
-			res.json(data.results[0]);
-		}
-	});
+exports.pois = function(req, res){
+	res.json({"success": "ok"});
 };
-
-exports.propertyDetails = function(req, res){
-	console.log('property view called');
-	var address = req.params.address;
-	getProperty(address, function(err,data){
-		if(err){
-			console.log(err);
-		}
-		else{
-			res.json(data);
-		}
-	});
-};
-
-function getProperty(address, callback){
-	var	addr = address.split(','), 
-		citystatezip = addr.splice(1); 
-		path = '/webservice/GetDeepSearchResults.htm?zws-id=' + ZWSID + '&address=' 
-			+ encodeURIComponent(addr[0]) + '&citystatezip=' + encodeURIComponent(citystatezip),
-		options = {
-	  host: 'www.zillow.com',
-	  path: path
-	};
-	
-	http.get(options, function (http_res) {
-	    // initialize the container for our data
-	    var data = "";
-
-	    // this event fires many times, each time collecting another piece of the response
-	    http_res.on("data", function (chunk) {
-	        // append this chunk to our growing `data` var
-	        data += chunk;
-	    });
-
-	    // this event fires *one* time, after all the `data` events/chunks have been gathered
-	    http_res.on("end", function () {
-	        // you can use res.send instead of console.log to output via express
-	       	parser.parseString(data, function(err, result){
-						if(!err){
-							//console.log(result);
-							callback(null,result);
-						}
-					});
-	    });
-	
-			http_res.on('error', function(e){
-				callback(e);
-			});
-	});
-}
-
-function getGraphsAndData(id, callback){
-	var	addr = address.split(','), 
-		citystatezip = addr.splice(1); 
-		path = '/webservice/GetDeepSearchResults.htm?zws-id=' + ZWSID + '&address=' 
-			+ encodeURIComponent(addr[0]) + '&citystatezip=' + encodeURIComponent(citystatezip),
-		options = {
-	  host: 'www.zillow.com',
-	  path: path
-	};
-	
-	http.get(options, function (http_res) {
-	    // initialize the container for our data
-	    var data = "";
-
-	    // this event fires many times, each time collecting another piece of the response
-	    http_res.on("data", function (chunk) {
-	        // append this chunk to our growing `data` var
-	        data += chunk;
-	    });
-
-	    // this event fires *one* time, after all the `data` events/chunks have been gathered
-	    http_res.on("end", function () {
-	        // you can use res.send instead of console.log to output via express
-	       	parser.parseString(data, function(err, result){
-						if(!err){
-							//console.log(result);
-							callback(null,result);
-						}
-					});
-	    });
-	
-			http_res.on('error', function(e){
-				callback(e);
-			});
-	});
-}
