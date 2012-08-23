1d80be53083f5561f65f591f8fb9ad207ed93f03
Keith Elliott <mrkeithelliott@gmail.com>
2012-08-23 14:36:40 -0400

    add spies and expectation matcher tests

--- spec/geostats_spec.js
+++ spec/geostats_spec.js
@@ -28,4 +28,77 @@ describe('A geostats obj', function(){
 		expect(poi).toBeDefined();
 		expect(poi.name).toEqual('Work2');
 	});
+	
+	// Additional Expectation Matchers
+	it("the 'toContain' matcher finds an item in an array", function(){
+		var geeks = ['Keith', 'Tim', 'Wil', 'Brett'];
+		
+		expect(geeks).toContain('Wil');
+		expect(geeks).not.toContain('Patrick');
+	});
+	
+	
+	// toBeLessThan comparison
+	it("can determine if less than", function(){
+		var a = 300.87, b = 500;
+		
+		expect(a).toBeLessThan(b);
+		expect(b).not.toBeLessThan(a);
+	});
+	
+	// toBrGreaterThan comparison
+	it("can determine if greater than", function(){
+		var a = 300.87, b = 500;
+		
+		expect(b).toBeGreaterThan(a);
+		expect(a).not.toBeGreaterThan(b);
+	});
+	
+	// can use precision matchers
+	it("can use precision matchers", function(){
+		var sillyNum = 5.87867, sillyOtherNum = 5.879;
+		
+		expect(sillyNum).toBeCloseTo(sillyOtherNum,0.001);
+	});
+});
+
+
+// using spies
+describe('tests with spies!', function(){
+	var geek,foundOne, geeks;
+	
+	beforeEach(function(){
+		geek = {
+			addAGeek: function(g){
+				if(typeof geeks === 'undefined'){
+					geeks = [];
+				}
+				geeks.push(g);
+			},
+			findAGeek: function(g){
+				return 'you are it buddy!';
+			}
+		};
+		
+		afterEach(function(){
+			geek = null;
+			foundOne = null;
+			geeks = null;
+		});
+		
+		spyOn(geek,'addAGeek').andCallThrough();
+		spyOn(geek,'findAGeek').andCallThrough();
+		
+		geek.addAGeek('Brett');
+		foundOne = geek.findAGeek('Brett');
+	});
+	
+	it('track that findAGeek was called', function(){
+		expect(geek.addAGeek).toHaveBeenCalled();
+	});
+	
+	it('should return a geek', function(){
+		expect(geek.findAGeek).toHaveBeenCalled();
+		expect(foundOne).toEqual('you are it buddy!');
+	})
 });
